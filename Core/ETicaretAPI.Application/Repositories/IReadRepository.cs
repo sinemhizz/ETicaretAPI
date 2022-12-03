2267a496c01db80f599cabbbdbd5fa1b9b1f5b34
sinemhizzz <sinem.hiz@d1-tech.com>
2022-12-03 18:59:44 +0300

    Add project files.

---
+++ Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IReadRepository<T> : IRepository<T> where T :BaseEntity
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method); //şarta uygun olan tüm verileri getirir.
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method); //şarta uygun ilk veriyi getirir.
+        Task<T> GetByIdAsync(string id);
+    }
+}
